b432cccb1511d5156bbe607cfb4c7445a7a4f526
Vivek <saivivekv95@gmail.com>
2026-01-14 21:07:42 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Vivek",
+    author_email="saivivekvi95@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
